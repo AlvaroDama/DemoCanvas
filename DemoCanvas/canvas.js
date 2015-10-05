63cb8db67e259c5cd9dc9085a752e2146738c271
Álvaro <alvaro.d.jimenez@gmail.com>
2015-10-05 11:48:21 +0200

    Pruebas con Canvas HTML5

---
+++ DemoCanvas/canvas.js
@@ -0,0 +1,14 @@
+﻿var canvas = document.getElementById("miCanvas");
+var ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "#ccff66";
+ctx.fillRect(10, 30, 100, 100);
+
+ctx.strokeStyle = "#66ff66";
+ctx.moveTo(50, 30);
+ctx.lineTo(250, 197);
+ctx.stroke();
+
+ctx.strokeStyle = "red";
+ctx.font = "40px Arial";
+ctx.strokeText("¡Hey There!", 50, 50);
